7195caee34814c24cf69ecadb8f04a9ba6550afe
brianway <250902678@qq.com>
2016-12-28 11:24:50 +0800

    [add] add AddCommissionedEmployee

---
+++ asd4j-payroll/src/main/java/com/brianway/asd4j/payroll/CommissionedClassification.java
@@ -0,0 +1,30 @@
+package com.brianway.asd4j.payroll;
+
+/**
+ * Created by brian on 16/12/27.
+ */
+public class CommissionedClassification extends PaymentClassification {
+    private double salary;
+    private double commissionRate;
+
+    public CommissionedClassification(double salary, double commissionRate) {
+        this.salary = salary;
+        this.commissionRate = commissionRate;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public double getCommissionRate() {
+        return commissionRate;
+    }
+
+    public void setCommissionRate(double commissionRate) {
+        this.commissionRate = commissionRate;
+    }
+}
